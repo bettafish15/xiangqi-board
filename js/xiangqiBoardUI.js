c416894880c3019e2d8c5fd7814b0c8b63c7d1f2
nguyen hoang hung <bettafish6@gmail.com>
2021-03-28 13:19:56 +0700

    initial commit

---
+++ js/xiangqiBoardUI.js
@@ -0,0 +1,17 @@
+;(function(){
+    $(document).ready(function() 
+    {
+
+        $('#xiangqiBoard').css({
+            'background': 'url("img/xiangqiboard.svg") no-repeat',
+            'border': '2px solid black'
+        });
+        $('#xiangqiBoard')
+        
+        
+
+        //window.xiangqiBoard = xiangqiBoard;  
+
+    });
+    
+})(); 
