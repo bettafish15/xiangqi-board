bed703fcefedc965328b402d7a1ffd4b71fc98db
bettafish15 <bettafish6@gmail.com>
2021-09-30 23:45:14 +0700

    create structure for the app

---
+++ prettier.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  singleQuote: true,
+  arrowParens: 'avoid',
+  tabWidth: 2,
+  jsxSingleQuote: false,
+  jsxBracketSameLine: false,
+  trailingComma: 'all',
+};
---
+++ src/components/common/Piece/Piece.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Piece: React.FC = () => <div className="App" />;
+
+export default Piece;
